dcda2a0fee10741ab2fb8cd8e2ef909b8601d29b
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-05-18 04:43:52 +0200

    Add testbench

--- pergola/applets/blinky/__init__.py
+++ pergola/applets/blinky/__init__.py
@@ -1,27 +1,79 @@
 from nmigen import *
+from nmigen.back.pysim import Active
 from .. import Applet
+
+
+class BlinkyImpl(Elaboratable):
+    description = "Blinks some LEDs"
+    help = "Blinks some LEDs"
+
+    def __init__(self, led, btn):
+        self.led = led
+        self.btn = btn
+        self.blink = Signal()
+        self.timer = Signal(24)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.d.sync += self.timer.eq(self.timer + 1)
+        m.d.comb += self.led.o.eq(self.blink)
+
+        with m.If(self.btn):
+            m.d.comb += self.blink.eq(1)
+        with m.Else():
+            m.d.comb += self.blink.eq(self.timer[-1])
+
+        return m
+
+
+class BlinkyTest(Elaboratable):
+    def __init__(self, args):
+        pass
+
+    def elaborate(self, platform):
+        from nmigen.compat.fhdl.specials import TSTriple
+        led = TSTriple()
+        btn = Signal()
+
+        m = Module()
+
+        self.tm = BlinkyImpl(led, btn)
+        m.submodules.blinky = self.tm
+
+        return m
+
+    def testbench(self, sim):
+        sim.add_clock(1e-6)
+
+        def loopback_proc():
+            yield Active()
+            assert (yield self.tm.timer) == 0
+            yield
+            assert (yield self.tm.timer) == 1
+            yield
+            assert (yield self.tm.timer) == 2
+            yield
+            assert (yield self.tm.timer) == 3
+            yield
+
+        sim.add_sync_process(loopback_proc)
+
 
 class Blinky(Applet, applet_name="blinky"):
     description = "Blinks some LEDs"
     help = "Blinks some LEDs"
+    test_class = BlinkyTest
 
     def __init__(self, args):
-        self.blink = Signal()
+        pass
 
     def elaborate(self, platform):
-        led   = platform.request("led", 0)
-        btn   = platform.request("button", 0)
-        timer = Signal(24)
+        led = platform.request("led", 0)
+        btn = platform.request("button", 0)
 
         m = Module()
 
-        m.d.sync += timer.eq(timer + 1)
-        m.d.comb += led.o.eq(self.blink)
-
-        with m.If(btn):
-            m.d.comb += self.blink.eq(1)
-        with m.Else():
-            m.d.comb += self.blink.eq(timer[-1])
+        m.submodules.blinky = BlinkyImpl(led, btn)
 
         return m
-
--- pergola/cli.py
+++ pergola/cli.py
@@ -1,20 +1,36 @@
+import logging
 import argparse
 from argparse import RawTextHelpFormatter
+from nmigen.back.pysim import Simulator
 from .applets import *
 from .platform.pergola import PergolaPlatform
 
+logger = logging.getLogger(__name__)
+
+def run_test(applet_cls, args):
+    logger.debug("Running test for '{}'".format(applet_cls.applet_name))
+    applet = applet_cls.test_class(args=args)
+    sim = Simulator(applet)
+    applet.testbench(sim)
+    gtkwave = hasattr(args, "gtkwave") and args.gtkwave
+    vcd = hasattr(args, "vcd") and args.vcd
+
+    if vcd or gtkwave:
+        with sim.write_vcd("{}.vcd".format(args.applet), "{}.gtkw".format(args.applet)):
+            sim.run()
+    else:
+        sim.run()
+
+    if gtkwave:
+        import subprocess
+        subprocess.run(["gtkwave", "{}.gtkw".format(args.applet)])
+        print("done")
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v", "--verbose", default=0, action="count",
         help="increase logging verbosity")
-
-    parser.add_argument(
-        "--skip-prog", default=0, action="count",
-        help="skips the building and programming of gateware")
-
 
     subparsers = parser.add_subparsers(dest="action", metavar="COMMAND")
     subparsers.required = True
@@ -34,12 +50,7 @@ def main():
         description="Builds an applet bitstream and shows a graph of the design",
         help="builds an applet bitstream and shows a graph of the design")
 
-    p_test = subparsers.add_parser(
-        "test",
-        description="Builds and loads an applet bitstream",
-        help="builds and loads an applet bitstream")
-
-    for action_parser in [p_run, p_build, p_dot, p_test]:
+    for action_parser in [p_run, p_build, p_dot]:
         p_action_applet = action_parser.add_subparsers(dest="applet", metavar="APPLET")
         for applet in Applet.all.values():
             subparser = p_action_applet.add_parser(
@@ -50,18 +61,52 @@ def main():
             applet.add_build_arguments(subparser)
             applet.add_run_arguments(subparser)
 
-    args = parser.parse_args()
+    p_test = subparsers.add_parser(
+        "test",
+        description="Tests an applet",
+        help="tests an applet")
+    p_test_applet = p_test.add_subparsers(dest="applet", metavar="APPLET")
+    for applet in Applet.all.values():
+        subparser = p_test_applet.add_parser(
+            applet.applet_name,
+            description=applet.description,
+            help=applet.help,
+            formatter_class=RawTextHelpFormatter)
+        subparser.add_argument(
+            "--vcd", default=0, action="count",
+            help="Creates a .vcd and .gtkw file for waveform analysis")
+        subparser.add_argument(
+            "--gtkwave", default=0, action="count",
+            help="Opens gtkwave after the test has run")
+        applet.add_test_arguments(subparser)
 
-    applet_cls = Applet.all[args.applet]
-    platform = PergolaPlatform()
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
     if args.action == "run":
+        applet_cls = Applet.all[args.applet]
+        platform = PergolaPlatform()
         platform.build(applet_cls(args=args), do_program=True)
     if args.action == "build":
+        applet_cls = Applet.all[args.applet]
+        platform = PergolaPlatform()
         platform.build(applet_cls(args=args), do_program=False)
     if args.action == "dot":
+        applet_cls = Applet.all[args.applet]
+        platform = PergolaPlatform()
         platform.build(applet_cls(args=args), do_program=False, yosys_opts="-p show")
     elif args.action == "test":
-        print("Not implemented.")
+        if not args.applet:
+            for applet in Applet.all:
+                applet_cls = Applet.all[applet]
+                if hasattr(applet_cls, "test_class"):
+                    run_test(applet_cls, args)
+                else:
+                    print("WARN: {} is missing a test class".format(applet))
+        else:
+            applet_cls = Applet.all[args.applet]
+            run_test(applet_cls, args)
 
 if __name__ == "__main__":
     main()
