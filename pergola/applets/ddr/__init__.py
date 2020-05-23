6ffd3f4eac263e385f05de21da40ef33d7c4b1b1
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-05-23 13:52:07 +0200

    Add argument for ddrx2

--- pergola/applets/ddr/__init__.py
+++ pergola/applets/ddr/__init__.py
@@ -4,10 +4,9 @@ from nmigen.build import *
 from .. import Applet
 
 
-class DDRExample(Elaboratable):
-    def __init__(self, ddr1x_led, ddr2x_led):
-        self.ddr1x_led = ddr1x_led
-        self.ddr2x_led = ddr2x_led
+class DDRx1Example(Elaboratable):
+    def __init__(self, ddrx1_led):
+        self.ddrx1_led = ddrx1_led
         self.timer = Signal(25)
 
     def elaborate(self, platform):
@@ -15,34 +14,56 @@ class DDRExample(Elaboratable):
 
         m.d.sync += self.timer.eq(self.timer + 1)
 
-        m.d.comb += self.ddr1x_led.o_clk.eq(self.timer[-1])
-        m.d.comb += self.ddr1x_led.o0.eq(0b1)
-        m.d.comb += self.ddr1x_led.o1.eq(0b0)
+        m.d.comb += self.ddrx1_led.o_clk.eq(self.timer[-1])
+        m.d.comb += self.ddrx1_led.o0.eq(0b1)
+        m.d.comb += self.ddrx1_led.o1.eq(0b0)
 
-        m.d.comb += self.ddr2x_led.o_clk.eq(self.timer[-1])
-        m.d.comb += self.ddr2x_led.o_eclk.eq(self.timer[-2])
-        m.d.comb += self.ddr2x_led.o0.eq(0b1)
-        m.d.comb += self.ddr2x_led.o1.eq(0b0)
-        m.d.comb += self.ddr2x_led.o2.eq(0b1)
-        m.d.comb += self.ddr2x_led.o3.eq(0b0)
+        return m
+
+class DDRx2Example(Elaboratable):
+    def __init__(self, ddrx2_led):
+        self.ddrx2_led = ddrx2_led
+        self.timer = Signal(25)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.d.sync += self.timer.eq(self.timer + 1)
+
+        m.d.comb += self.ddrx2_led.o_clk.eq(self.timer[-1])
+        m.d.comb += self.ddrx2_led.o_eclk.eq(self.timer[-2])
+        m.d.comb += self.ddrx2_led.o0.eq(0b1)
+        m.d.comb += self.ddrx2_led.o1.eq(0b0)
+        m.d.comb += self.ddrx2_led.o2.eq(0b1)
+        m.d.comb += self.ddrx2_led.o3.eq(0b0)
 
         return m
 
 
 class DDRExampleApplet(Applet, applet_name="ddr"):
-    description = "DDR example (xdr=4 requires a patched nmigen)"
+    description = "DDR example (ddrx2 requires my branch of nmigen for the time being)"
     help = "DDR example"
 
+
+    @classmethod
+    def add_run_arguments(cls, parser):
+        parser.add_argument(
+            "--ddrx2", default=0, action="count",
+            help="Use ddrx2 / qdr / ODDRX2F")
+
     def __init__(self, args):
+        self.ddrx2 = args.ddrx2
         pass
 
     def elaborate(self, platform):
-        ddr1x_led = platform.request("led", 0, xdr=2)
-
-        ddr2x_led = platform.request("led", 7, xdr=4)
 
         m = Module()
 
-        m.submodules.ddr_example = DDRExample(ddr1x_led, ddr2x_led)
+        ddrx1_led = platform.request("led", 0, xdr=2)
+        m.submodules.ddrx1_example = DDRx1Example(ddrx1_led)
+
+        if self.ddrx2:
+            ddrx2_led = platform.request("led", 7, xdr=4)
+            m.submodules.ddrx2_example = DDRx2Example(ddrx2_led)
 
         return m
