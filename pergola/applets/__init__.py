9aa81718de474fb482958fe0be41b58b9fdc7296
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-06-01 18:21:23 +0200

    Sort applets

--- pergola/applets/__init__.py
+++ pergola/applets/__init__.py
@@ -34,8 +34,8 @@ class Applet(Elaboratable):
 
 from . import blinky
 from . import delayf
-from . import xdr
 from . import hdmi
 from . import hdmi_splitter
 from . import pll
 from . import radio_tx
+from . import xdr
